f71289b95f5ea6fb0385a689fde3494ab350d6cc
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-01-07 17:25:06 +0100

    Add basic CLI input reading

--- src/cli.c
+++ src/cli.c
@@ -1,5 +1,19 @@
 #include "cli.h"
 
 void cli_start() {
-    printf("Hello CLI\n");
+    ssize_t nread;
+    char* line = NULL;
+    size_t len = 0;
+
+    while((nread = getline(&line, &len, stdin)) != -1) {
+        line[nread - 1] = 0;
+        struct parse_result result = parse(line);
+
+        if(result.error) {
+            printf("%s\n", result.error);
+            continue;
+        }
+    }
+
+    free(line);
 }
--- src/cli.h
+++ src/cli.h
@@ -1,8 +1,12 @@
 #ifndef CLI_H
 #define CLI_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
 
 void cli_start();
+void run_command(struct parse_result* result);
 
 #endif
