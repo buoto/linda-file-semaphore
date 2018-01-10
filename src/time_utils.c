3525748ff3c53a9cfb63edf53901e6e26d8dc998
Michał Błotniak <buoto336@gmail.com>
2018-01-10 22:42:55 +0100

    Add add_ms function

--- src/time_utils.c
+++ src/time_utils.c
@@ -7,11 +7,34 @@ struct timespec from_ms(unsigned ms) {
     };
 }
 
+struct timespec get_now() {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ts;
+}
+
+struct timespec add_ms(const struct timespec *ts, long ms) {
+    long nsec = ts != NULL ? ts->tv_nsec : 0;
+    nsec += ms * NS_IN_MS; // add millis
+
+    time_t sec = ts != NULL ? ts->tv_sec : 0;
+    sec += nsec / NS_IN_S; // handle overflow
+    nsec = nsec % NS_IN_S;
+
+    return (struct timespec) {
+        .tv_sec = sec,
+        .tv_nsec = nsec,
+    };
+}
+
 time_t sem_timedwait_ms(sem_t *sem, time_t timeout_ms) {
     struct timespec start, end, ts = from_ms(timeout_ms);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    sem_timedwait(sem, &ts);
+    printf("%d %d\n",ts.tv_sec, ts.tv_nsec);
+    int x = sem_timedwait(sem, &ts);
+    printf("sem %d\n", sem);
+    perror();
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     time_t diff_sec = end.tv_sec - start.tv_sec;
--- src/time_utils.h
+++ src/time_utils.h
@@ -6,9 +6,16 @@
 #include <semaphore.h>
 #include <time.h>
 
+#define NS_IN_MS 1000000
+#define NS_IN_S  1000000000
+#define MS_IN_S  1000
+
 struct timespec from_ms(unsigned ms);
 
-time_t sem_timedwait_ms(sem_t *sem, time_t timeout_ms);
+struct timespec get_now();
 
+struct timespec add_ms(const struct timespec *ts, long ms);
+
+time_t sem_timedwait_ms(sem_t *sem, time_t timeout_ms);
 
 #endif /* end of include guard: TIME_UTILS_H */
