a5649d15db7e78323de77fbd19b936fedbe5f56c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-01-11 01:47:40 +0100

    copy paste linda read into linda input

--- src/linda.c
+++ src/linda.c
@@ -61,8 +61,96 @@ int linda_input(
     struct tuple* output,
     unsigned timeout_ms
 ) {
-    printf("linda_input: not implemented/n");
-    exit(1);
+    // sanity checks
+    if(l == NULL || output == NULL || pattern == NULL) {
+        return -1;
+    }
+
+    const struct file *linda_file = &(l->file);
+
+    // get start value
+    struct timespec start;
+    if(set_now(&start) == -1) {
+        return errno;
+    }
+    struct timespec deadline = add_ms(&start, timeout_ms); // deadline
+
+    while(1) {
+        if(sem_timedwait(l->reader_mutex, &deadline)) { // wait(reader_mutex)
+            return errno; // error or timeout - abort
+        }
+        // reader_mutex PROTECTED
+        sem_post(l->readers_count); // readers_count ++
+        sem_post(l->reader_mutex); // signal(reader_mutex);
+        // reader_mutex END
+
+        struct store s = make_store();
+
+        timed_lock(linda_file, &deadline); // rlock
+        // file_lock PROTECTED
+        int err = read_store_file(linda_file, &s); // operate
+        unlock(linda_file); // runlock
+        // file_lock END
+
+        if(err) {
+            /* Wrapping below trywait in mutex is unnecessary, because process can
+             * either decrements what it has incremented or his increment was
+             * consumed by someone else and trywait does nothing */
+            sem_trywait(l->readers_count); // prompt decrement
+            return err; // errors from read_store_file
+        }
+
+        struct tuple *target_tuple = find_in_store(&s, *pattern, match_tuple);
+        destroy_store(&s);
+
+        if(target_tuple != NULL) {
+            // SUCCESS - tuple is found
+            sem_trywait(l->readers_count); // prompt decrement
+            *output = *target_tuple;
+            return 0;
+        }
+
+        if(sem_timedwait(l->notify, &deadline)) { // wait(notify)
+            sem_trywait(l->readers_count); // prompt decrement
+            return errno;
+        }
+
+        if(sem_timedwait(l->reader_mutex, &deadline)) { // wait(reader_mutex)
+            sem_trywait(l->readers_count); // prompt decrement
+            return errno;
+        }
+        // reader_mutex PROTECTED
+        sem_trywait(l->readers_count); // readers_count--
+
+        if(sem_trywait(l->readers_count)) { // readers_count == 0 ?
+            if(errno != EAGAIN) {
+                // something really bad happened
+                sem_post(l->reader_mutex);
+                return errno;
+            }
+            // semaphore is closed => readers_count == 0
+            // the last reader arrived, unblock all done_reading
+            int done_reading_count;
+            sem_getvalue(l->done_reading, &done_reading_count);
+            while(done_reading_count < 0) { // count is negative or zero
+                sem_post(l->done_reading);
+                sem_getvalue(l->done_reading, &done_reading_count);
+            }
+
+            sem_post(l->reader_mutex);
+            // reader_mutex END1
+        } else {
+            // early arrivers here
+            sem_post(l->readers_count); // revert value after measurment
+
+            sem_post(l->reader_mutex); // return the mutex
+            // reader_mutex END2
+
+            if(sem_timedwait(l->done_reading, &deadline)) { // wait for the others
+                return errno;
+            }
+        }
+    }
 }
 
 int linda_read(
